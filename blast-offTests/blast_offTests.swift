a77531c0f98c9d8b7b26756beca3faa894b21d78
George Baines <g.h.baines@hotmail.com>
2015-04-11 13:40:39 +0100

    Initial Commit

---
+++ blast-offTests/blast_offTests.swift
@@ -0,0 +1,36 @@
+//
+//  blast_offTests.swift
+//  blast-offTests
+//
+//  Created by George Baines on 11/04/2015.
+//  Copyright (c) 2015 George Baines. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class blast_offTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
