cc3323b8232e6479ac27b068cc45d33dea999e21
George Baines <g.h.baines@hotmail.com>
2015-04-12 10:04:34 +0100

    About to make big changes. I might break everything.

--- blast-off/FirstViewController.swift
+++ blast-off/FirstViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    // full path for local file data.plist
+    //var documentDir = String()
+    //var path = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        
+        
+        
+        //documentDir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        //path = documentDir.stringByAppendingPathComponent("UserData.plist")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -26,8 +36,26 @@ class FirstViewController: UIViewController {
     var counter = 0;
     
     func update(){
-        counter++
-        println(counter)
+        //([[0, 0]] as NSArray).writeToFile(path, atomically: true)
+        // read the file
+        /* Note: assumes file is already present */
+        //var data = NSArray(contentsOfFile: path) as [[Int:Int]]
+        
+        //var userMoney = 0
+        //var userResource = 0
+        
+        //for ( money, resources ) in data[0]{
+        //    userMoney = money
+        //    userResource = resources
+        //}
+
+        // append to the file
+        
+        // write to the file
+        //([userMoney++, userResource] as NSArray).writeToFile(path, atomically: true)
+        
+        //counter++
+        //println(counter)
     }
 
 }
